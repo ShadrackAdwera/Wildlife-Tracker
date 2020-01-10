892de2a3fa9d298ff3a7e5fad03a6e117a2c82f1
ADWESH <adweshshaddie@gmail.com>
2020-01-10 12:36:07 +0300

    Added a child class for animals: Endengered Animals and inherited from the animal class

---
+++ src/main/java/EndangeredAnimal.java
@@ -0,0 +1,14 @@
+public class EndangeredAnimal extends Animal {
+    private static final String HEALTH_ILL = "ill";
+    private static final String HEALTH_HEALTHY = "healthy";
+    private static final String HEALTH_OKAY = "okay";
+    private static final String AGE_YOUNG = "young";
+    private static final String AGE_NEWBORN = "newborn";
+    private static final String AGE_ADULT = "adult";
+
+    public EndangeredAnimal(String name, int id){
+        super(name, id);
+    }
+
+
+}
