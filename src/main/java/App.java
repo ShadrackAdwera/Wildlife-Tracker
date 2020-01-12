2fb5789b88da93872aa158e81667252e47b108b6
ADWESH <adweshshaddie@gmail.com>
2020-01-12 09:32:25 +0300

    Added a view hbs and form to process animals that are not endangered

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -27,6 +27,8 @@ public class App {
             model.put("sighting", sightings);
             List<EndangeredAnimal> animals = EndangeredAnimal.all();
             model.put("animals", animals);
+            List<NotEndangeredAnimal> notEndangeredAnimals = NotEndangeredAnimal.all();
+            model.put("not-endangered-animals", notEndangeredAnimals);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine() );
         //display location form
@@ -36,6 +38,8 @@ public class App {
             model.put("sightings", sightings);
             List<EndangeredAnimal> animals = EndangeredAnimal.all();
             model.put("animals", animals);
+            List<NotEndangeredAnimal> notEndangeredAnimals = NotEndangeredAnimal.all();
+            model.put("not-endangered-animals", notEndangeredAnimals);
             return new ModelAndView(model, "location.hbs");
         }, new HandlebarsTemplateEngine());
         //post: process location form
@@ -55,6 +59,8 @@ public class App {
             model.put("sightings", sightings);
             List<EndangeredAnimal> animals = EndangeredAnimal.all();
             model.put("animals", animals);
+            List<NotEndangeredAnimal> notEndangeredAnimals = NotEndangeredAnimal.all();
+            model.put("not-endangered-animals", notEndangeredAnimals);
             int idOfFoundSite = Integer.parseInt(request.params("id"));
             Sighting foundSite = Sighting.find(idOfFoundSite);
             model.put("sights", foundSite);
@@ -70,6 +76,8 @@ public class App {
             model.put("sightings", sightings);
             List<EndangeredAnimal> animals = EndangeredAnimal.all();
             model.put("animals", animals);
+            List<NotEndangeredAnimal> notEndangeredAnimals = NotEndangeredAnimal.all();
+            model.put("not-endangered-animals", notEndangeredAnimals);
             return new ModelAndView(model,"animal.hbs");
         }, new HandlebarsTemplateEngine());
         //post: process animals form
@@ -85,6 +93,30 @@ public class App {
             return null;
         }, new HandlebarsTemplateEngine());
 
+        //display not endangered animal form
+        get("/not-endangered-animal/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Sighting> sightings = Sighting.all();
+            model.put("sightings", sightings);
+            List<EndangeredAnimal> animals = EndangeredAnimal.all();
+            model.put("animals", animals);
+            List<NotEndangeredAnimal> notEndangeredAnimals = NotEndangeredAnimal.all();
+            model.put("not-endangered-animals", notEndangeredAnimals);
+            return new ModelAndView(model,"not-endangered.hbs");
+        }, new HandlebarsTemplateEngine());
+        //post: process not endangered animals form
+        post("/not-endangered", (request, response) -> {
+            Map <String, Object> model = new HashMap<>();
+            String animalName = request.queryParams("name");
+            String animalAge = request.queryParams("age");
+            String animalLocation = request.queryParams("animalLocation");
+            String animalHealth = request.queryParams("healthStatus");
+            NotEndangeredAnimal animal = new NotEndangeredAnimal(animalName, animalAge, animalLocation, animalHealth, 1);
+            animal.save();
+            response.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
         //get: view an animal
         get("/location/:locationId/animal/:animalId", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
@@ -92,6 +124,8 @@ public class App {
             model.put("sighting", sightings);
             List<EndangeredAnimal> animals = EndangeredAnimal.all();
             model.put("animals", animals);
+            List<NotEndangeredAnimal> notEndangeredAnimals = NotEndangeredAnimal.all();
+            model.put("not-endangered-animals", notEndangeredAnimals);
             int idAnimalToFind = Integer.parseInt(request.params("animalId"));
             EndangeredAnimal foundAnimal = EndangeredAnimal.find(idAnimalToFind);
             int locationFound = Integer.parseInt(request.params("locationId"));
@@ -100,6 +134,24 @@ public class App {
             model.put("sighting", foundSite);
             model.put("sightings", Sighting.all());
             return new ModelAndView(model, "animal-detail.hbs");
+        }, new HandlebarsTemplateEngine());
+        //get: view not endangered animal
+        get("/location/:locationId/animal/:animalId", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Sighting> sightings = Sighting.all();
+            model.put("sighting", sightings);
+            List<EndangeredAnimal> animals = EndangeredAnimal.all();
+            model.put("animals", animals);
+            List<NotEndangeredAnimal> notEndangeredAnimals = NotEndangeredAnimal.all();
+            model.put("not-endangered-animals", notEndangeredAnimals );
+            int idAnimalToFind = Integer.parseInt(request.params("animalId"));
+            NotEndangeredAnimal foundAnimal = NotEndangeredAnimal.find(idAnimalToFind);
+            int locationFound = Integer.parseInt(request.params("locationId"));
+            Sighting foundSite = Sighting.find(locationFound);
+            model.put("not-endangered-animal", foundAnimal);
+            model.put("sighting", foundSite);
+            model.put("sightings", Sighting.all());
+            return new ModelAndView(model, "not-endangered-details.hbs");
         }, new HandlebarsTemplateEngine());
 
     }
